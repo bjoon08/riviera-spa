b981e27d81f5378d28cae7881fcae6a04e68eae2
Kevinjeet <kgill2992@gmail.com>
2024-05-13 15:07:38 -0700

    added docker and made edits to files so docker compose up works

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('./models/User.js');
+const User = require('../models/User.js');
 
 // Signup route
 router.post('/signup', async (req, res) => {
